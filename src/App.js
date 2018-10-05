9d2093141d2b5228dfc55cfdce21c98fc293a905
t1gor <igor.timoshenkov@gmail.com>
2018-10-05 12:45:34 +0300

    Add a login to React app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo               from './logo.svg';
 import './App.css';
+import { VXPay, VXPayConfig } from 'vxpay-js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			vxpay:      new VXPay(new VXPayConfig(window)),
+			isLoggedIn: false,
+			username: '',
+		};
+	}
+
+
+	componentWillMount() {
+		this.openVxPay = this.openVxPayHandler.bind(this);
+		this.showProgress = this.showProgressHandler.bind(this);
+		this.onLogin = this.onLoginHandler.bind(this);
+
+		// add hooks
+		this.state.vxpay.hooks.then(hooksConfig => {
+			hooksConfig.onLogin(this.showProgress);
+			hooksConfig.onSuccess(this.onLogin);
+		})
+	}
+
+	openVxPayHandler() {
+		this.state.vxpay.openLogin();
+	}
+
+	/**
+	 * [@param {VXPayLoggedInMessage} msg]
+	 */
+	showProgressHandler() {
+		this.setState({ isLoggedIn: this.state.vxpay.isLoggedIn() });
+	}
+
+	/**
+	 * @param {VXPaySuccessMessage} message
+	 */
+	onLoginHandler(message) {
+		this.setState({ username: message.user.nickname });
+	}
+
+	render() {
+		const userPanel = this.state.isLoggedIn
+			? <span>Hello, {this.state.username}</span>
+			: <a className="App-link" onClick={this.openVxPay} style={{ cursor: "pointer" }}>Login</a>;
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Very simple <code>vxpay-js</code> React demo.</p>
+					{userPanel}
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
