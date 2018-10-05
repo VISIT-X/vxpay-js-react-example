44762d787bd52ab6deffc6eefbbe95a8c3a1740b
t1gor <igor.timoshenkov@gmail.com>
2018-10-05 13:35:32 +0300

    Updates

---
+++ precache-manifest.614b4ecdc973e1b0c1c5071d56fc4b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vxpay-js-react-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3ee1edf912019d3d8253",
+    "url": "/vxpay-js-react-example/static/js/runtime~main.3ee1edf9.js"
+  },
+  {
+    "revision": "bfe6b45ec81ca1040ef0",
+    "url": "/vxpay-js-react-example/static/js/main.bfe6b45e.chunk.js"
+  },
+  {
+    "revision": "868d700f3d749c5b20fa",
+    "url": "/vxpay-js-react-example/static/js/1.868d700f.chunk.js"
+  },
+  {
+    "revision": "bfe6b45ec81ca1040ef0",
+    "url": "/vxpay-js-react-example/static/css/main.868560c4.chunk.css"
+  },
+  {
+    "revision": "5f27278fb0b8ae167b912f6062bf1131",
+    "url": "/vxpay-js-react-example/index.html"
+  }
+];
